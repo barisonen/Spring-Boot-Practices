d104b567a44de025725915feae429b7d3a27c1e0
barisonen <barisonen.1996@gmail.com>
2022-12-26 21:35:40 +0300

    JWT with Spring Security

---
+++ jwt/jwt/src/main/java/com/example/jwt/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.jwt.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+}
---
+++ jwt/jwt/src/main/java/com/example/jwt/security/CustomUserDetails.java
@@ -0,0 +1,42 @@
+package com.example.jwt.security;
+
+import com.example.jwt.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails extends User implements UserDetails {
+
+    public CustomUserDetails() {}
+
+    public CustomUserDetails(User user) {
+        this.setId(user.getId());
+        this.setUsername(user.getUsername());
+        this.setPassword(user.getPassword());
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
