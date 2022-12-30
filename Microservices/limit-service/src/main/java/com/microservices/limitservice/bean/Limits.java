8bab4a4b6ab6beb696159af70c33482f4f2078df
barisonen <barisonen.1996@gmail.com>
2022-12-30 22:38:31 +0300

    Limits microservice added.

---
+++ Microservices/limit-service/src/main/java/com/microservices/limitservice/bean/Limits.java
@@ -0,0 +1,12 @@
+package com.microservices.limitservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Limits {
+
+    private int minimum;
+    private int maximum;
+}
---
+++ Microservices/limit-service/src/main/java/com/microservices/limitservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.microservices.limitservice.controller;
+
+import com.microservices.limitservice.bean.Limits;
+import com.microservices.limitservice.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
