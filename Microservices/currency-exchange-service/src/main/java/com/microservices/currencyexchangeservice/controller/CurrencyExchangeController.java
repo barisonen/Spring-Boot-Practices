8960b5b7ce1209dd879457071bce6f7905f7ec3d
barisonen <barisonen.1996@gmail.com>
2022-12-31 23:06:29 +0300

    Jpa added to Currency Exchange service

--- Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.microservices.currencyexchangeservice.controller;
 
-import com.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import com.microservices.currencyexchangeservice.entity.CurrencyExchange;
+import com.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +16,19 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
 
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
 
--- Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/entity/CurrencyExchange.java
+++ Microservices/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/entity/CurrencyExchange.java
@@ -1,6 +1,8 @@
-package com.microservices.currencyexchangeservice.bean;
+package com.microservices.currencyexchangeservice.entity;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,10 +10,16 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
