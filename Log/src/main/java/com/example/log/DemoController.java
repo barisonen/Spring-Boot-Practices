1e503879b85b0c86d9439983291609ad83b46e3a
barisonen <barisonen.1996@gmail.com>
2023-02-24 17:02:11 +0300

    Log4j Time Based Rolling

---
+++ Log/src/main/java/com/example/log/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.log;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+public class DemoController {
+
+    @GetMapping("/log-something")
+    public void logSomething() {
+        log.error("some error");
+        log.info("some info");
+        log.debug("some debug");
+        log.fatal("some fatal");
+    }
+}
