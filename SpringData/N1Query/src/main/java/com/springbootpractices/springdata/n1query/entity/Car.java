a4e8abf10ba8fac079005d235bb944bef6a6e96b
barisonen <barisonen.1996@gmail.com>
2023-03-11 23:08:56 +0300

    n+1 query project started

---
+++ SpringData/N1Query/src/main/java/com/springbootpractices/springdata/n1query/entity/Car.java
@@ -0,0 +1,29 @@
+package com.springbootpractices.springdata.n1query.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Owner owner;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+}
